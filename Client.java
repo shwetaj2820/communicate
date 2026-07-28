2fc9efd3731c55c43d80ff77169df42977aa917e
shwetaj2820 <shwetajoshi446@gmail.com>
2026-07-29 02:24:41 +0530

    write client code

---
+++ Client.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+import java.net.InetAddress;
+
+public class Client {
+    public void run() throws IOException{
+        int port = 8080;
+        InetAddress client = InetAddress.getByName("localhost");
+
+        try(
+            Socket clientSocket = new Socket(client, port);
+            PrintWriter sendData = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader buffer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+        ){
+            Scanner input = new Scanner(System.in);
+
+            while(true){
+                System.err.println("send next request to server");
+                sendData.println(input.nextLine());
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        Client runClient = new Client();
+        runClient.run();
+    }
+}
